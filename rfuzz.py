c3def29843a9b1d14b2ea2ca2fffbf4d27bd141f
Timur Nebesniy <0x4380@users.noreply.github.com>
2022-11-17 16:42:20 +0300

    Update rfuzz.py
    Exceptions for get_async()

--- rfuzz.py
+++ rfuzz.py
@@ -8,7 +8,7 @@ import httpx
 def silent_remove(filename):
     try:
         os.remove(filename)
-    except OSError as e:  # this would be "except OSError, e:" before Python 2.6
+    except OSError as e:  
         if e.errno != errno.ENOENT:  # errno.ENOENT = no such file or directory
             raise  # re-raise exception if a different error occurred
 
@@ -23,8 +23,7 @@ def save_to_file(link_list, filename):
 
 
 def url_list_gen():
-  # TODO: make domains.lst and routes.lst as input in args for rfuzz.py 
-    urls = ['https://test.com'] # check outgoing connection first
+    urls = ['https://google.com'] #First URL to check
     with open("domains.lst") as domain_list:
         for domain in domain_list:
             domain = domain.strip()
@@ -38,23 +37,29 @@ def url_list_gen():
 
 async def get_async(url):
     async with httpx.AsyncClient() as client:
-        return await client.get(url)
+        try:
+            print('Checking URL: ', url)
+            return await client.get(url, follow_redirects=False)
+        except httpx.RequestError as exc:
+            print(f"\t\tERROR occurred while requesting {exc.request.url!r}.")
+        except httpx.HTTPStatusError as exc:
+            print(f"\t\tERROR response {exc.response.status_code} while requesting {exc.request.url!r}.")
+        except:
+            print(f"Error sending request for ", url)
 
 
 async def launch():
+    tm1 = time.perf_counter()
     list_200 = ['']
     url_list = url_list_gen()
-    resps = await asyncio.gather(*map(get_async, url_list))
-    for resp in resps:
-        if resp.status_code == 200:
-            print('FOUND!\t\t' + resp.request.url + '\t 200')
-            list_200.append(resp.request.url + '\t 200')
+    results = await asyncio.gather(*map(get_async, url_list))
+    for r in results:
+        if r.status_code == 200:
+            print('FOUND!\t\t' + r.request.url + '\t 200')
+            list_200.append(r.request.url + '\t 200')
     save_to_file(list_200, 'results_200.txt')
+    tm2 = time.perf_counter()
+    print(f'Total time elapsed: {tm2 - tm1:0.2f} seconds')
 
-
-tm1 = time.perf_counter()
 
 asyncio.run(launch())
-
-tm2 = time.perf_counter()
-print(f'Total time elapsed: {tm2 - tm1:0.2f} seconds')
