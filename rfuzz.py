a766a8731d4fe9e01a7e3579aa264218cbf84f2e
Timur Nebesniy <0x4380@users.noreply.github.com>
2022-12-03 10:57:31 +0300

    Update rfuzz.py
    Turned off SSL validation due to race condition error in ssl.py

--- rfuzz.py
+++ rfuzz.py
@@ -2,11 +2,29 @@ import time
 import aiohttp
 import asyncio
 import logging
+import os, errno
 
 # 861.71 seconds for 9*400 URLs
 logging.basicConfig(filename='debug.log', encoding='utf-8', level=logging.DEBUG,
                     format='%(asctime)s.%(msecs)03d %(levelname)s %(module)s - %(funcName)s: %(message)s',
                     datefmt='%Y-%m-%d %H:%M:%S', )
+
+
+def silent_remove(filename):
+    try:
+        os.remove(filename)
+    except OSError as e:
+        if e.errno != errno.ENOENT:  # errno.ENOENT = no such file or directory
+            raise  # re-raise exception if a different error occurred
+
+
+def save_to_file(list_to_save, filename):
+    silent_remove(filename)
+    with open(filename, mode='wt', encoding='utf-8') as result:
+        result.write('\n'.join(list_to_save))
+        result.write('\n')
+        result.close()
+    return 'ok'
 
 
 def count_lines():
@@ -36,7 +54,6 @@ async def get_lines(lines_count, domain_name):
                 url = f"https://{domain_name}/{s.strip()}"
                 final_list.append(url)
                 s_counter += 1
-            print(f"\tURLS for domain {domain_name}: {s_counter}/{total}")
             lines_start = lines_end
             lines_end += iter_size
             slice_lines.clear()
@@ -44,7 +61,10 @@ async def get_lines(lines_count, domain_name):
         async with aiohttp.ClientSession() as session:
             for full_url in final_list:
                 try:
-                    async with session.get(full_url, allow_redirects=False, timeout=3) as resp:
+                    # Turned off SSL validation for speed up
+                    # And to skip race condition in ssl.py/sslproto.py
+                    async with session.get(full_url, allow_redirects=False, timeout=15,
+                                           ssl=False) as resp:
                         try:
                             url_status = resp.status
                             print(f"{url_status}\t\t{full_url}")
@@ -56,25 +76,37 @@ async def get_lines(lines_count, domain_name):
                             raise
                 except:
                     logging.error('Failed session.get(full_url) ' + full_url)
-                    print(('Failed session.get(full_url) ' + full_url))
                     raise
 
     except:
         logging.error(f'Failed async with session')
         raise
     print(f"\n\tREADY:")
-    print(list_200)
+    print(f"{list_200}\n\n")
+    result_name = domain_name + ".result"
+    save_to_file(list_200, "results/" + result_name)
     return list_200  # Do something
 
 
 async def walk_list():
-    with open("domains.lst") as domain_list:
-        time_start = time.perf_counter()
-        for domain in domain_list:
-            domain_name = domain.strip()
-            await get_lines(100, domain_name)
-        time_done = time.perf_counter()
-        print(f'Total time elapsed: {time_done - time_start:0.2f} seconds.')
+    try:
+        with open("domains.lst") as domain_list:
+            time_start = time.perf_counter()
+            for domain in domain_list:
+                domain_name = domain.strip()
+                try:
+                    await get_lines(100, domain_name)
+                except:
+                    pass
+            time_done = time.perf_counter()
+            print(f'Total time elapsed: {time_done - time_start:0.2f} seconds.')
+    except:
+        logging.error(f"walk_list error")
+        pass
 
 
-asyncio.run(walk_list())
+try:
+    asyncio.run((walk_list()), debug=True)
+except:
+    ('some error here? ')
+    raise
